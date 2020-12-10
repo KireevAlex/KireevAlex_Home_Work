80e4eccaf74d7d093827e8d6a8588ba6c04965e1
KireevAlex <kireeva.a91@gmail.com>
2020-12-10 13:46:32 +0200

    fix the slide

--- HW_13_2.js
+++ HW_13_2.js
@@ -1,23 +1,4 @@
 
-
-// 1. Исправить работу слайда 29, сделать что 
-// бы выводилась последовательность 
-// от 1 до 10 двумя разными способами без ЕС6
-
-for (var i = 0; i < 10; i++) {
-  function index(i){
-    console.log(i)
-  };
-  setTimeout(index ,3000, i+1)
-  
-}
-
-for (var i = 0; i < 10; i++) {
-  setTimeout(function(i) { 
-    console.log(i);
-      },5000, i+1)
-}
-
 // 2. Написать цепочку вызовов математических вычислений
 // math.sum().sum().minus().multiplay().showResult().
 
